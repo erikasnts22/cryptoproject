ceb33ded16f540a268dafb204a203448d2b525e8
“erikasnts22” <sntserika22@gmail.com>
2021-10-01 11:22:57 +0800

    commits everything

--- Gemfile
+++ Gemfile
@@ -22,7 +22,10 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'jquery-rails'
-gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+#gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+gem 'bootstrap'
+gem 'devise'
+gem 'rename'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,42 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
 
+    #@url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=e75d37d1-b7b2-4253-9f36-289eaff48122'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=e75d37d1-b7b2-4253-9f36-289eaff48122&start=1&limit=5000&convert=USD'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
+  end
+
+  def lookup
+    require 'net/http'
+    require 'json'
+
+    #@url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=e75d37d1-b7b2-4253-9f36-289eaff48122'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=e75d37d1-b7b2-4253-9f36-289eaff48122&start=1&limit=5000&convert=USD'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin = JSON.parse(@response)
+
+    @symbol = params[:sym]
+
+    #error handling on lookup form
+    #changing the input text to uppercase to match on key value pair of JSON on API
+    if @symbol
+          @symbol = @symbol.upcase
+    end
+
+    #if entered an empty value
+    if @symbol == ""
+        @symbol = "Please Enter A Currency!"
+    end
   end
 
   def about
   end
+
 
 end
